fb4b29e7a6ea8d439074bdc5483cfba2f1d60d2d
RianB99 <31669566+RianB99@users.noreply.github.com>
2020-12-14 23:31:22 +0100

    dynamic building id's given

--- components/base_map.py
+++ components/base_map.py
@@ -7,6 +7,8 @@ from bokeh.models import ColumnDataSource, TapTool, MultiPolygons, Patches, Sele
 from bokeh.io import curdoc
 from bokeh import events
 
+from ast import literal_eval
+
 from pyproj import Transformer, transform
 
 TRAN_4326_TO_3857 = Transformer.from_crs("EPSG:4326", "EPSG:3857")
@@ -56,38 +58,51 @@ def add_public_transport(fig):
 
 def draw_polygon(fig):
     # load data about buildings
-    # df = pd.read_csv("./data/Amsterdam_20panden.csv")
-    
-    # test data for a house/building
-    coordinates = [[52.31960, 4.81197], [52.31961, 4.81205], [52.31968, 4.81202], [52.31967, 4.81194], [52.31960, 4.81197]]
-    coordinates = [TRAN_4326_TO_3857.transform(coord[0], coord[1]) for coord in coordinates]
-    
-    # needs to be done for every building
-    x_coords = [[[[c[1] for c in coordinates]]]]
-    y_coords = [[[[c[0] for c in coordinates]]]]
-    print(x_coords, y_coords)
+    df = pd.read_csv("./data/test.csv")
+    print(len(df))
 
-    data = {'xs': x_coords, 'ys': y_coords}
+    x_coords = []
+    y_coords = []
+
+    for i in range(len(df)):
+        coords = df.iloc[i]['WGS']
+        coords = literal_eval(coords)
+        coords = [TRAN_4326_TO_3857.transform(float(coord[0]), float(coord[1])) for coord in coords]
+        x_coords.append([[[c[1] for c in coords]]])
+        y_coords.append([[[c[0] for c in coords]]])
+
+    # # # test data for a house/building
+    # coordinates = [[52.31960, 4.81197], [52.31961, 4.81205], [52.31968, 4.81202], [52.31967, 4.81194], [52.31960, 4.81197]]
+    # coordinates = [TRAN_4326_TO_3857.transform(coord[0], coord[1]) for coord in coordinates]
+
+    # # # needs to be done for every building
+    # x_coords.append([[[c[1] for c in coordinates]]])
+    # y_coords.append([[[c[0] for c in coordinates]]])
+    # print("x_coords:", x_coords)
+    # print("y_coords:", y_coords)
+
+    data = {'xs': x_coords, 'ys': y_coords, 'id':list(df['pand_id'])}
+    names = list(df['pand_id'])
 
     s1 = ColumnDataSource(data=data)
 
     # name is id number of building, tags to identify type of glyph
-    glyph = fig.multi_polygons(xs='ys', ys='xs', color="navy", name="5", source=s1)
-    glyph.tags = ["pand"]
+    ### can't get name to be dynamic, defines it for every column
+    glyph = fig.multi_polygons(xs='ys', ys='xs', color="navy", name="pand", source=s1, alpha=0.3)
 
     # what happens in the call
-    call = CustomJS(code="""
-            let id = cb_obj.renderers[0].name; 
-            console.log(id);
+    call = CustomJS(args=dict(source=s1), code="""
+            /* console.log(cb_data.source.selected.indices[0]); */
+
+            let idx = cb_data.source.selected.indices[0];
+            let pand_id = source.data.id[idx];
+            console.log(pand_id);
 
             /* here comes ajax callback*/
-            $.ajax({
-                type:'GET',
-                url:('/building/' + id)
-            }).done(console.log("ajax request done"))
+            window.location = ("http://127.0.0.1:5000/building/" + pand_id)
             """)
 
-    # option 3, only polygons are clickable !!!!!
+    # only make polygons clickable
     tap = TapTool(renderers=[glyph], callback=call)
     fig.tools.append(tap)
 
